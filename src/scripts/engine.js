7f2006822bcdb5ca33f2f03d6a95f6d3bd3872ce
Luiz Guilherme <contato.guilhermealmeida@gmail.com>
2023-11-28 17:38:14 -0300

    add img and sounds initial engine

---
+++ src/scripts/engine.js
@@ -0,0 +1,19 @@
+const state ={
+    view: {
+        squares: document.querySelectorAll(".square"),
+        enemy: document.querySelectorAll(".enemy"),
+        score: document.querySelector("#score"),
+        timeLeft: document.querySelector("#time-left")
+    },
+    values: {},
+};
+
+function addListenerHitBox(){
+    state.view.squares.forEach((square) =>{
+        if(square.id === 1)
+    });
+}
+
+function init(){}
+
+init();
